524b253377742087c1493211491aca295ded2a77
JonShepChen <SharpShooter787@gmail.com>
2015-07-11 09:58:39 -0700

    Get input loop working

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from curses import textpad
 import time
 import curses
 
@@ -9,8 +10,10 @@ class MusicPlayer(object):
 
     def run(self, stdscreen):
         self.setUpWindows(stdscreen)
+        self.runLoop(stdscreen)
 
     def setUpWindows(self, stdscreen):
+        self.master_screen = stdscreen
         track_list_length = 120
         track_list_height = 40
 
@@ -18,24 +21,44 @@ class MusicPlayer(object):
         play_list_height = 40
 
         search_buffer_length = 100
-        search_buffer_height = 0
+        search_buffer_height = 1
 
         curr_playing_length = 100
         curr_playing_height = 2
 
         self.track_list_subwin = stdscreen.subwin(track_list_height, track_list_length, 0, 0)
         self.playlist_subwin = stdscreen.subwin(play_list_height, play_list_length, 0, self.track_list_subwin.getmaxyx()[1]+1)
-        self.search_subwin = stdscreen.subwin(search_buffer_height, search_buffer_length, self.playlist_subwin.getmaxyx()[0]+1, 0)
+        self.search_subwin = stdscreen.subwin(search_buffer_height, search_buffer_length, self.track_list_subwin.getmaxyx()[0]+1, 0)
         self.curr_playing_subwin = stdscreen.subwin(curr_playing_height, curr_playing_length, stdscreen.getmaxyx()[0]-2, 0)
+        self.search_text_box = textpad.Textbox(self.search_subwin)
+        self.search_text_box.stripspaces = 1
 
-        #for index in range(0,track_list_height):
-        #    self.track_list_subwin.addstr(index, 0, "hop")
-        #    self.playlist_subwin.addstr(index, 0, "blahh")
+    def runLoop(self, stdscreen):
+        search_key = 115
+        quit_key = 113
+        move_up_key = curses.KEY_UP
+        move_down_key = curses.KEY_DOWN
+        next_song_key = curses.KEY_RIGHT
+        prev_song_key = curses.KEY_LEFT
 
-        #self.search_subwin.addstr(0, 0, "DAMNNN")
-        #self.curr_playing_subwin.addstr(0, 0, "HERRR")
-        #stdscreen.refresh()
-        #time.sleep(5)
+        #welcomescreen
+
+        while True:
+            self.search_subwin.clear()
+            user_input = self.search_text_box.edit()
+            self.track_list_subwin.addstr(0, 0, user_input)
+            self.track_list_subwin.refresh()
+
+
+    def getQueryResults(self):
+        pass
+
+    def printTrackList(self):
+        pass
+
+    def printPlaylistList(self):
+        pass
+
 
 
 
