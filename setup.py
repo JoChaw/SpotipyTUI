7f419dcf9ac1dcf08209427407d0a1b2f9481318
JonShepChen <SharpShooter787@gmail.com>
2015-08-02 10:12:05 -0700

    Update setup for v1.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
         name = 'spotipy-tui',
         packages = ['spotipy_tui'],
-        version = '1.0.4',
+        version = '1.0.5',
         include_package_data = True,
         description = 'Text-based UI to control Spotify client',
         author = 'Jonathan Chen',
@@ -11,7 +11,7 @@ setup(
         license='MIT',
         entry_points={'console_scripts': ['spotipy-tui=spotipy_tui.app:run']},
         url = 'https://github.com/JonShepChen/SpotipyTUI',
-        download_url = 'https://github.com/JonShepChen/SpotipyTUI/archive/v1.0.4.tar.gz',
+        download_url = 'https://github.com/JonShepChen/SpotipyTUI/archive/v1.0.5.tar.gz',
         keywords = ['spotify', 'remote', 'audio', 'music', 'tui', 'curses'],
         install_requires=['requests'],
         classifiers = [
